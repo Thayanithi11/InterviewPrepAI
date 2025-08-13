8cbd1bcb538e976f071ef3e4061a347b8a94081f
Thayanithi <thayanithi452004@gmail.com>
2025-08-14 00:25:59 +0530

    done with home and blog page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import {Toaster} from "react-hot-toast";
-
-
+import Home from './pages/Home';
+import Blog from './pages/Blog';
 
 function App() {
   return (
     <div>
-    
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/blog/:id' element={<Blog/>}/>
+      </Routes>
     </div>
   )
 } 
