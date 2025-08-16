f5b4af8afc6501c65a837474b97b5b410cc5fc33
Thayanithi <thayanithi452004@gmail.com>
2025-08-17 02:13:56 +0530

    blogpage completed

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,21 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import { assets,blog_data} from '../assets/assets'
+import { assets,blog_data, comments_data} from '../assets/assets'
 import Moment from 'moment'
+import Footer from '../components/Footer'
+import Loader from '../components/Loader'
 
 function Blog() {
 
-    const {id}=useParams()
-    const[data,setData]=useState(null)
+    const {id}=useParams();
+    const[data,setData]=useState(null);
+    const[comments,setCommets]=useState([]);
+    const[name,setName]=useState('');
+    const[content,setContent]=useState('');
 
     const fetchBlogData=async ()=>{
         const data=blog_data.find(item=> item._id===id)
         setData(data)
     }
 
+    const fetchComments=async ()=>{
+      setCommets(comments_data)
+    }
+
+    const addComment=()=>{
+
+    }
+
     useEffect(()=>{
         fetchBlogData()
+        fetchComments()
     },[])
 
   return data ? (
@@ -37,8 +51,53 @@ function Blog() {
           <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML={{__html:data.description}}></div>
        </div>
 
+       <div className="mt-14 mb-10 max-w-3xl mx-auto">
+        <p className='font-semibold mb-4'>Comments ({comments.length})</p>
+        <div className="flex flex-col gap-4">
+          {comments.map((item,index) => {
+          return (
+           <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600'>
+             <div className='flex items-center gap-2 mb-2'>
+               <img src={assets.user_icon} alt="" className='w-6'/>
+               <p className='font-medium'>{item.name}</p>
+             </div>
+             <p className='text-sm max-w-md ml-8'>{item.content}</p>
+             <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>
+               {Moment(item.createdAt).fromNow()}
+             </div>
+           </div>
+          )  })}
+        </div>
+       </div>
+          {/* Add comment section */}
+       <div className='max-w-3xl mx-auto'>
+          <p className='font-semibold mb-4'>Add your comment</p>
+          <form  onSubmit={addComment} className='flex flex-col items-start
+          gap-4 max-w-lg'>
+            <input type="text" placeholder='Name'required className='w-full
+            p-2 border border-gray-300 rounded outline-none' onChange={(e)=>setName(e.target.value)} value={name}/>
+
+            <textarea placeholder='Comment' className='w-full p-2 border
+            border-gray-300 rounded outline-none h-48' required onChange={(e)=>setContent(e.target.value)} value={content}></textarea>
+
+            <button type='submit' className='bg-primary text-white rounded p-2 mb-4
+            px-8 hover:scale-110 transition-all curson-pointer'>Submit</button>
+          </form>
+       </div>
+
+       {/* share buttons */}
+       <div className='my-8 max-w-3xl mx-auto'>
+         <p className='font-semibold my-4'>Share this article on social media</p>
+         <div className='flex'>
+          <img src={assets.facebook_icon} width={50} />
+          <img src={assets.twitter_icon} width={50} />
+          <img src={assets.googleplus_icon} width={50} />
+         </div>
+       </div>
+
+      <Footer/>
     </div>
-  ):<div>Loading</div>
+  ):(<Loader/>)
 }
 
 export default Blog
