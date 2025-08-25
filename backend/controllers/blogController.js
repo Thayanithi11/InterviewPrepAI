86128082ff6e7bd4807ee7a54e89d3b0ae632482
Thayanithi <thayanithi452004@gmail.com>
2025-08-25 21:34:05 +0530

    all api's have been written

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import imagekit from '../configs/imageKit.js';
 import Blog from '../models/Blog.js'
+import Comment from '../models/Comment.js'
 
 export const addBlog = async (req,res)=>{
     try{
@@ -40,3 +41,80 @@ export const addBlog = async (req,res)=>{
         res.json({success:false,message:err.message})
     }
 }
+
+export const getAllBlogs=async (req,res)=>{
+    try{
+        const blogs=await Blog.find({isPublished:true})
+        res.json({success:true,blogs})
+    }
+    catch(err){
+        res.json({success:false,message:err.message})
+    }
+}
+
+export const getBlogById=async (req,res)=>{
+    try{
+      const {blogId}=req.params
+      const blog=await Blog.findById(blogId)
+      if(!blog)
+      {
+        res.json({sucess:false,message:"Blog not found"})
+      }
+      res.json({success:true,blog})
+    }
+    catch(err)
+    {
+         res.json({success:false,message:err.message})
+    }
+}
+
+export const deleteBlogById=async (req,res)=>{
+    try{
+      const {id}=req.body;
+      await Blog.findByIdAndDelete(id)
+      //Delete comments as well
+      await Comment.deleteMany({blog:id})
+      res.json({success:true,message:"Blog deleted successfully"})
+    }
+    catch(err)
+    {
+         res.json({success:false,message:err.message})
+    }
+}
+
+export const togglePublish = async (req,res)=>{
+    try{
+    const {id}=req.body;
+    const blog= await Blog.findById(id)
+    blog.isPublished=!blog.isPublished
+    blog.save()
+    res.json({success:true,message:'Blog status Updated'})
+    }
+    catch(err){
+        res.json({success:false,message:err.message})
+    }
+}
+
+
+export const addComment=async (req,res)=>{
+    try{
+    const {blog,name,content}=req.body
+    await Comment.create({blog,name,content})
+    res.json({success:true,message:'Comment added successfully'})
+   }
+   catch(err){
+     res.json({success:false,message:err.message})
+   }
+}
+
+export const getBlogComments=async (req,res)=>{
+    try{
+        const {blogId}=req.body
+        const comments=await Comment.find({blog:blogId,isApproved:true}).sort({createdAt:-1})
+        res.json({success:true,comments})
+    }
+    catch(err){
+        res.json({success:false,message:err.message})
+    }
+}
+
